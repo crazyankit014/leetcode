fe5cd013ffea5097c2a15c88a2f183c0ab6ae4a3
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-04-24 19:22:01 +0530

    Added solution - LeetHub

---
+++ Pattern 14 - GFG/pattern-14.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+         for(int i=0;i<n;i++)
+         {
+             for(char ch='A';ch<='A'+i;ch++)
+             {
+                 cout<<ch;
+             }
+             cout<<endl;
+         }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
