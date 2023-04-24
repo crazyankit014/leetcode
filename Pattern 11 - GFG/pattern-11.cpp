1714f7167657ca50f56254ad758406d9fe6e7fd1
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-04-24 18:56:15 +0530

    Added solution - LeetHub

--- Pattern 11 - GFG/pattern-11.cpp
+++ Pattern 11 - GFG/pattern-11.cpp
@@ -7,18 +7,26 @@ using namespace std;
 class Solution {
   public:
     void printTriangle(int n) {
-        int start=1;
-     for(int i=0;i<n;i++)
-     {
-         if(i%2==0) start=1;
-         else start=0;
-         for(int j=0;j<=i;j++)
-         {
-             cout<<start<<" ";
-             start=1-start;
-         }
-         cout<<endl;
-     }
+    //     int start=1;
+    //  for(int i=0;i<n;i++)
+    //  {
+    //      if(i%2==0) start=1;
+    //      else start=0;
+    //      for(int j=0;j<=i;j++)
+    //      {
+    //          cout<<start<<" ";
+    //          start=1-start;
+    //      }
+    //      cout<<endl;
+    //  }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<(i+j+1)%2<<" ";
+        }
+        cout<<endl;
+    }
     }
 };
 
