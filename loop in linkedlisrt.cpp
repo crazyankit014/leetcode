00a5891de4ac83a7ad5a52120ae615b61d704e80
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-05-03 21:24:12 +0530

    Create loop in linkedlisrt.cpp

---
+++ loop in linkedlisrt.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL) return false;
+         ListNode* slow=head;
+         ListNode* fast=head;
+          while(fast->next && fast->next->next)
+          {
+              slow=slow->next;
+              fast=fast->next->next;
+              if(slow==fast)
+                return true;
+              
+          }
+          return false;
+    }
+};
