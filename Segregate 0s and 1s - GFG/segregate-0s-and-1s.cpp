c480924578e892b0e7ddff6c6365fde367a2d92f
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-10-26 01:52:45 +0530

    Added solution - LeetHub

--- Segregate 0s and 1s - GFG/segregate-0s-and-1s.cpp
+++ Segregate 0s and 1s - GFG/segregate-0s-and-1s.cpp
@@ -10,7 +10,31 @@ using namespace std;
 class Solution{   
 public:
     void segregate0and1(int arr[], int n) {
-        sort(arr,arr+n);
+        // sort(arr,arr+n);
+        
+        int count0=0;
+        int count1=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {
+                count0++;
+            }
+            else 
+            {
+                count1++;
+            }
+        }
+        
+        for(int i=0;i<count0;i++)
+        {
+            arr[i]=0;
+        }
+        
+        for(int i=count0;i<n;i++)
+        {
+            arr[i]=1;
+        }
     }
 };
 
